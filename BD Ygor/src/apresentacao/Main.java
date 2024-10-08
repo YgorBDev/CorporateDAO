33f6bd1ab394982725c4601c2396c7b9f0c8e122
Ygor <ygorb.dev@gmail.com>
2024-10-08 12:05:19 -0300

    Update Main.java

--- BD Ygor/src/apresentacao/Main.java
+++ BD Ygor/src/apresentacao/Main.java
@@ -12,32 +12,26 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Scanner teclado = new Scanner(System.in);
-			
-			BancoDeDados bd = new BancoDeDados();
 
+			BancoDeDados bd = new BancoDeDados();
+	
 			while (true) {
-				
-				System.out.println("\n== Cadastro Empresarial ==");
-				System.out.println("1) Cadastrar uma Empresa");
-				System.out.println("2) Mostrar Empresas Cadastradas");
-				System.out.println("3) Contratar Funcionarios");
-				System.out.println("4) Mostrar Contratos ");
-				System.out.println("5) Declarar Impostos");
-				System.out.println("6) Mostrar Declaraçoes");
-				System.out.println("7) Opção Secret");
-				
+	
+				System.out.println("\n== Cadastro Tabajara ==");
+				System.out.println("1) Cadastrar uma pessoa");
+				System.out.println("2) Mostrar o cadastro");
+				System.out.println("3) Encerrar");
 				System.out.print("Escolha uma opção: ");
 				int opcao = teclado.nextInt();
 				teclado.nextLine();
-
 				if (opcao == 1) {
 					System.out.print("\nDigite o nome da pessoa: ");
 					String nome = teclado.nextLine();
 					System.out.print("Digite o CPF da pessoa: ");
 					String cpf = teclado.nextLine();
 					System.out.print("Digite a data de nascimento da pessoa (YYYY-MM-DD): ");
-					String DataNascimento = teclado.nextLine();
-					Pessoa p = new Pessoa(nome, cpf, DataNascimento);
+					String dataNasc = teclado.nextLine();
+					Pessoa p = new Pessoa(nome, cpf, dataNasc);
 					try {
 						bd.armazenarPessoa(p);
 					} catch (SQLIntegrityConstraintViolationException e) {
@@ -47,7 +41,6 @@ public class Main {
 					}
 					System.out.println("\nUsuário cadastrado com sucesso!");
 				}
-				
 				else if (opcao == 2) {
 					ArrayList<Pessoa> lista = bd.getPessoas();
 					if (lista.size() == 0) {
@@ -59,11 +52,9 @@ public class Main {
 						}
 					}
 				}
-				
 				else if (opcao == 3) {
 					break;
 				}
-				
 				else {
 					System.out.println("Opção inválida!");
 				}
