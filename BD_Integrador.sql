80e6b8f87693e3fedfa0bf8ba94530b69bef760a
Ygor <ygorb.dev@gmail.com>
2024-10-08 11:41:35 -0300

    Update BD_Integrador.sql

--- BD_Integrador.sql
+++ BD_Integrador.sql
@@ -2,41 +2,41 @@ DROP DATABASE IF EXISTS bd_integrador;
 CREATE DATABASE bd_integrador; 
 USE bd_integrador;
 
-
 CREATE TABLE empresa (
     id_empresa INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
     nome_empresa VARCHAR(255) NOT NULL,
     cnpj CHAR(15) NOT NULL UNIQUE,
-    lugar VARCHAR(255) NOT NULL unique,
+    lugar VARCHAR(255) NOT NULL UNIQUE,
     data_criacao DATE NOT NULL,
     lucro_bruto DECIMAL(15, 2) NOT NULL, 
-    lucro_liquido DECIMAL(15, 2) NOT NULL, 
-    imposto DECIMAL(15, 2) NOT NULL, 
+    lucro_liquido DOUBLE, 
+    imposto DOUBLE, 
+    qta_funcionario INT NOT NULL,
     status ENUM('ativa', 'inativa') DEFAULT 'ativa'
 );
 
 CREATE TABLE funcionario (
     id_funcionario INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
     id_empresa INT,
-    nome_funcionario VARCHAR(50) NOT NULL unique,
+    nome_funcionario VARCHAR(50) NOT NULL UNIQUE,
     cpf CHAR(14) NOT NULL UNIQUE,
-    imposto int,
+    valor_imposto DOUBLE,
     data_nasc DATE NOT NULL,
     cargo VARCHAR(50) NOT NULL,
     salario DECIMAL(10, 2) NOT NULL,  
-    celular VARCHAR(15) NOT NULL unique,     
+    celular VARCHAR(15) NOT NULL UNIQUE,     
     data_admissao DATE NOT NULL,
     status ENUM('ativo', 'inativo') DEFAULT 'ativo',
-    FOREIGN KEY (id_empresa) REFERENCES empresa(id)
+    FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
 );
 
 CREATE TABLE imposto (
     id_imposto INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
     id_empresa INT,
     ano INT NOT NULL,
-    valor_imposto DECIMAL(15, 2) NOT NULL, 
-    data_declaracao DATE NOT NULL,
-   );
+    imposto DOUBLE, 
+    data_declaracao DATE NOT NULL
+);
 
 create table paga(
 	id_paga INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
@@ -46,5 +46,8 @@ create table paga(
 	FOREIGN KEY (id_imposto) REFERENCES imposto(id_imposto)
 );
 
-SELECT * FROM empresa,funcionario,imposto;
+select*from empresa;
+select*from funcionario;
+select*from imposto;
+select*from paga;
 
