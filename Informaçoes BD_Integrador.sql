dcd7a83d718dd1aafc81cadd8db75bbd1ddfc956
Ygor <ygorb.dev@gmail.com>
2024-10-15 11:26:37 -0300

    Update Informaçoes BD_Integrador.sql

--- Informaçoes BD_Integrador.sql
+++ Informaçoes BD_Integrador.sql
@@ -1,13 +1,15 @@
 USE bd_integrador;
+
 INSERT INTO funcionario (nome_funcionario, cpf, data_nasc, cargo, salario, celular, data_admissao, status) VALUES
 ('João Silva', '123.456.789-01', '1985-06-15', 'Gerente', 5000.00, '11987654321', '2020-01-10', 'ativo'),
 ('Maria Oliveira', '987.654.321-00', '1990-12-22', 'Analista', 3500.00, '11912345678', '2021-03-15', 'ativo'),
 ('Carlos Santos', '135.792.468-00', '1988-07-30', 'Desenvolvedor', 4000.00, '11999998888', '2022-05-20', 'ativo');
 
-INSERT INTO empresa (nome_empresa, cnpj, lugar, data_criacao, lucro_bruto, lucro_liquido, imposto, status) VALUES
-('Tech Solutions', '123.456/0001-95', 'São Paulo', '2015-04-01', 200000.00, 150000.00, 20000.00, 'ativa'),
-('Consultoria ABC', '987.432/0001-96', 'Rio de Janeiro', '2018-08-15', 150000.00, 100000.00, 15000.00, 'ativa'),
-('InovaCorp', '159.753/0001-97', 'Belo Horizonte', '2020-10-10', 80000.00, 50000.00, 8000.00, 'ativa');
+INSERT INTO empresa (nome_empresa, cnpj, lugar, data_criacao, lucro_bruto, imposto, status) VALUES
+('Tech Solutions', '123.456/0001-95', 'São Paulo', '2015-04-01', 200000.00, 20000.00,'ativa'),
+('Consultoria ABC', '987.432/0001-96', 'Rio de Janeiro', '2018-08-15', 150000.00, 15000.00,'ativa'),
+('InovaCorp', '159.753/0001-97', 'Belo Horizonte', '2020-10-10', 80000.00, 8000.00, 'ativa');
+
 
 INSERT INTO imposto (ano, valor_imposto, data_declaracao) VALUES
 (2022, 20000.00, '2023-02-15'),
